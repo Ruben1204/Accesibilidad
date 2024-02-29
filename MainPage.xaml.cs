559299f42c08ffe43d3553759e02b56a0e6f422b
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2024-02-29 09:49:12 -0500

    FIrst Commit
    implementation from accesibility in NETMAUI

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Accesibilidad
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //label.SetSemanticFocus();
+            //SemanticScreenReader.Default.Announce("Este es un anuncio de texto");
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
